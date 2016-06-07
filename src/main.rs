0625f1ba3d2c1c5fd9305bda01d5711466672f22
Daniel Rodgers-Pryor <djrodgerspryor@gmail.com>
2016-06-08 00:27:31 +1000

    Allow prisoner simulation to be configured with CLI args
    And allow simulations to be run multiple times to get average durations.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,55 @@
 extern crate rand;
+extern crate docopt;
+extern crate rustc_serialize;
 
 use rand::Rng;
+use docopt::Docopt;
 
-const PRISONER_COUNT: usize = 100;
-const LOG_PERIOD: u32 = 1000;
+
+// Write the Docopt usage string.
+const USAGE: &'static str = "
+Usage:
+    illuminated-inmates [--prisoner-count=<prisoner-count>] [--repetitions=<repetitions>] [--log-period=<log-period>]
+
+Options:
+    -n, --prisoner-count=<prisoner-count>   Number of inmates to simulate [default: 100]
+    -r, --repetitions=<repetitions>         Number of times to repeat the simulation (for statistical analysis) [default: 1]
+    -l, --log-period=<log-period>           How frequently should the simulation log (in days) [default: 1000]
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    flag_prisoner_count: usize,
+    flag_repetitions: u32,
+    flag_log_period: u32,
+}
+
 
 #[derive(Clone)]
 struct Prisoner {
     known_visited_prisoners: Vec<bool>,
 }
 impl Prisoner {
-    fn new() -> Prisoner {
+    fn new(prisoner_count: usize) -> Prisoner {
         Prisoner {
-            known_visited_prisoners: vec![false; PRISONER_COUNT],
+            known_visited_prisoners: vec![false; prisoner_count],
         }
     }
 
     fn select_light_position(&mut self, day: u32, light_is_on: bool, self_index: usize) -> bool {
+        let prisoner_count = self.known_visited_prisoners.len();
+
         // I now know that I've been interrogated
         self.known_visited_prisoners[self_index] = true;
 
         // If the previous prisoner knew that this prisoner has been interrogated
         if day > 0 && light_is_on {
             // TODO: multiple strategies
-            self.known_visited_prisoners[((day - 1) as usize) % PRISONER_COUNT] = true;
+            self.known_visited_prisoners[((day - 1) as usize) % prisoner_count] = true;
         }
 
         // Return whether we know if today's prisoner has been interrogated
-        self.known_visited_prisoners[(day as usize) % PRISONER_COUNT]
+        self.known_visited_prisoners[(day as usize) % prisoner_count]
     }
 
     fn count_known(&self) -> u32 {
@@ -45,16 +67,17 @@ struct WorldState {
     light_is_on: bool,
 }
 impl WorldState {
-    fn new() -> WorldState {
+    fn new(prisoner_count: usize) -> WorldState {
         WorldState {
-            prisoners: vec![Prisoner::new(); PRISONER_COUNT],
+            prisoners: vec![Prisoner::new(prisoner_count); prisoner_count],
             day: 0,
             light_is_on: false,
         }
     }
 
     fn iterate(&mut self) -> bool {
-        let chosen_prisoner_index = rand::thread_rng().gen_range(0usize, PRISONER_COUNT);
+        let prisoner_count = self.prisoners.len();
+        let chosen_prisoner_index = rand::thread_rng().gen_range(0usize, prisoner_count);
         let mut chosen_prisoner = &mut self.prisoners[chosen_prisoner_index];
 
         self.light_is_on = chosen_prisoner.select_light_position(
@@ -64,7 +87,7 @@ impl WorldState {
         );
 
         // If this prisoner now knows that all prisoners have been interrogated
-        if chosen_prisoner.count_known() == (PRISONER_COUNT as u32) {
+        if chosen_prisoner.count_known() == (prisoner_count as u32) {
             return true; // Done
         }
 
@@ -81,14 +104,30 @@ impl WorldState {
     }
 }
 
-fn main() {
-    let mut state = WorldState::new();
+// Returns the duration in days
+fn run_simulation(prisoner_count: usize, log_period: u32) -> u32 {
+    println!("Beginning simulation with {} prisoners", prisoner_count);
+
+    let mut state = WorldState::new(prisoner_count);
 
     while !state.iterate() {
-        if state.day % LOG_PERIOD == 0 {
+        if state.day % log_period == 0 {
             println!("Day: {}, max-known: {}", state.day, state.best_known());
         }
     }
 
-    println!("Done! Day: {}", state.day);
+    println!("Done! Day: {}\n", state.day);
+
+    state.day
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
+
+    let simulation_results = (0..args.flag_repetitions)
+        .map(|_| run_simulation(args.flag_prisoner_count, args.flag_log_period))
+        .collect::<Vec<u32>>();
+
+    let average_runtime: u32 = simulation_results.iter().fold(0, |sum, x| sum + x) / args.flag_repetitions;
+    println!("Average run-time (over {} runs): {} days", args.flag_repetitions, average_runtime);
 }
